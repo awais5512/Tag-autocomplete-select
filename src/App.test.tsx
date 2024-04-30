d8f04bfe2bfd51cb2cd27e5211a715f3c4c80874
awais5512 <awais.abbas@emumba.com>
2024-04-30 13:01:12 +0500

    Setup project template

---
+++ src/App.test.tsx
@@ -0,0 +1,14 @@
+import App from './App';
+import { describe, it, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+
+test('demo', () => {
+  expect(true).toBe(true);
+});
+
+describe('render', () => {
+  it('renders the main page', () => {
+    render(<App />);
+    expect(true).toBeTruthy();
+  });
+});
