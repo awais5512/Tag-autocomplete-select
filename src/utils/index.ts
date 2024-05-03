7c07aea6e575b2f15544c735385d2a100209b3dc
awais5512 <awais.abbas@emumba.com>
2024-05-03 10:12:07 +0500

    Add named export for countries options to utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,11 @@
 export const generateRandomID = () => Math.floor(Math.random() * 1000);
+
+export const countriesOptions = [
+  { id: 0, text: 'Pakistan' },
+  { id: 1, text: 'Afghanistan' },
+  { id: 2, text: 'India' },
+  { id: 3, text: 'Australia' },
+  { id: 4, text: 'Bangladesh' },
+  { id: 5, text: 'Canada' },
+  { id: 6, text: 'Nepal' },
+];
