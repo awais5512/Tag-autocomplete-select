d18753be136e95fc3a59ecbff69a0c44fc675496
awais5512 <awais.abbas@emumba.com>
2024-05-08 14:07:13 +0500

    Fix test cases for tag input component in app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,61 @@
 import App from './App';
-import { describe, it, expect, test } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, expect, vitest, beforeEach } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import * as hooks from './hooks/useCountriesData';
+import userEvent from '@testing-library/user-event';
 
-test('demo', () => {
-  expect(true).toBe(true);
-});
+vitest.mock('./hooks/useCountriesData', () => ({
+  useCountriesData: vitest.fn(),
+}));
 
 describe('render', () => {
-  it('renders the main page', () => {
-    render(<App />);
-    expect(true).toBeTruthy();
+  beforeEach(() => {
+    vitest.clearAllMocks();
   });
 
-  it('should render correctly', async () => {
+  it('renders the main page without crashing', () => {
+    vitest.spyOn(hooks, 'useCountriesData').mockReturnValue({
+      loading: false,
+      error: null,
+      countries: [],
+    });
+
     render(<App />);
+
+    expect(screen.getByText(/Autocompletion Tag Input/i)).toBeInTheDocument();
+  });
+
+  it('allows users to select a country from autocomplete', async () => {
+    vitest.spyOn(hooks, 'useCountriesData').mockReturnValue({
+      loading: false,
+      error: null,
+      countries: [
+        { name: 'Afghanistan', iso2: 'AF', long: 65, lat: 33 },
+        { name: 'Albania', iso2: 'AL', long: 20, lat: 41 },
+      ],
+    });
+
+    render(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByTestId('tag-input')).toBeInTheDocument();
+    });
+
+    const expectedTaggedOption = 'Afghanistan';
+
+    const input = screen.getByTestId('tag-input');
+    userEvent.type(input, 'Afg');
+
+    await waitFor(() => {
+      expect(screen.getByText(expectedTaggedOption)).toBeInTheDocument();
+    });
+
+    userEvent.click(screen.getByText(expectedTaggedOption));
+
+    await waitFor(() => {
+      expect(screen.getAllByTestId('tagged-option')[0]).toBeInTheDocument();
+    });
+
+    expect(screen.getAllByTestId('tagged-option')[0].textContent).toBe(`${expectedTaggedOption}x`);
   });
 });
--- src/components/tagInput/tagInput.test.tsx
+++ src/components/tagInput/tagInput.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, expect, it, vitest } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { TagInput } from './tagInput';
 import { countriesOptions } from '../../utils';
 import { act } from 'react';
@@ -31,45 +31,4 @@ describe('TagInput', () => {
 
     expect(input.value).toEqual(inputText);
   });
-
-  it('Add option to tagged list when enter is pressed', async () => {
-    window.HTMLElement.prototype.scrollIntoView = function () {};
-
-    const addTagMock = vitest.fn();
-    const renderedTagInput = render(<TagInput tags={[]} autoCompleteOptions={countriesOptions} setTags={addTagMock} />);
-    const inputText = 'Afg';
-    const expectedTag = 'Afghanistan';
-    const input = (await renderedTagInput.findByTestId('tag-input')) as HTMLInputElement;
-
-    input.focus();
-
-    fireEvent.change(input, { target: { value: inputText } });
-    fireEvent.keyDown(input, { key: 'ArrowDown' });
-    fireEvent.keyDown(input, { key: 'Enter' });
-
-    await renderedTagInput.findByTestId('tagged-option');
-
-    expect(renderedTagInput.queryByText(expectedTag)).toBeTruthy();
-  });
-
-  // it('delete tag when clicked on x', async () => {
-  //   const addTagMock = vitest.fn();
-
-  //   render(<TagInput tags={[]} autoCompleteOptions={countriesOptions} setTags={addTagMock} />);
-
-  //   const input = (await screen.findByTestId('tag-input')) as HTMLInputElement;
-
-  //   input.focus();
-
-  //   act(() => {
-  //     fireEvent.change(input, { target: { value: 'Pak' } });
-  //   });
-
-  //   const firstSuggestion = await screen.findByText('Pakistan');
-  //   fireEvent.click(firstSuggestion);
-
-  //   screen.logTestingPlaygroundURL();
-  //   const deleteButton = await screen.findByTestId('tag-option-0-x');
-  //   expect(deleteButton).toBeInTheDocument();
-  // });
 });
